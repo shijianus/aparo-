2c0ece5e290351d989531cfe399d53c5364ad2f9
shijian <shijian.us@gmail.com>
2025-11-13 11:16:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ const PORT = process.env.SERVER_PORT || process.env.PORT || 3000;        // http
 const UUID = process.env.UUID || '0cb36d18-abec-4016-86fe-44855110cfac'; // 使用哪吒v1,在不同的平台运行需修改UUID,否则会覆盖
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
-const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';          // 固定隧道域名,留空即启用临时隧道
+const NEZHA_KEY = process.env.NEZHA_KEY || 'eyJhIjoiMWNiMTllMjM5YTEwZDQwZTljYjcwMGMxYTg0ZmQwYjkiLCJ0IjoiNTk2NmJjOTctM2M3ZC00ZWE4LWJiMzQtYmE5OGQzZjcxMThhIiwicyI6IlpqbGpZV016TkRndFpqTTRaaTAwWXpZeExXRm1aR1V0WkdVM1kyVmlNekF5T0RjNSJ9';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'nojs20251113.vedios.qzz.io';          // 固定隧道域名,留空即启用临时隧道
 const ARGO_AUTH = process.env.ARGO_AUTH || '';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
-const NAME = process.env.NAME || '';                        // 节点名称
+const NAME = process.env.NAME || 'hosting';                        // 节点名称
 
 // 创建运行文件夹
 if (!fs.existsSync(FILE_PATH)) {
